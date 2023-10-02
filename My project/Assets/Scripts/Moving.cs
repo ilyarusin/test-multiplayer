0bad8b858495b9962978e3414b231b168ef8d138
ilyarusin <ilyapiminov@gmail.com>
2023-10-02 20:05:22 +0500

    first commit
    project completed

---
+++ My project/Assets/Scripts/Moving.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+
+public class Moving : NetworkBehaviour
+{
+    Rigidbody rb;
+    public float speed = 5f;
+    public float rotationSpeed = 10f;
+    Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+        if (!base.IsOwner)
+            return;
+        
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 directionVector = new Vector3(-v, 0, h);
+
+        if (directionVector.magnitude > Mathf.Abs(0.05f))
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * rotationSpeed);
+
+        animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
+        rb.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
+    }
+}
---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 5f;
+    public float rotationSpeed = 10f;
+    Animator animator;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        /*
+        if (!base.IsOwner)
+            return;
+        */
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 directionVector = new Vector3(-v, 0, h);
+
+        if (directionVector.magnitude > Mathf.Abs(0.05f))
+            
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * rotationSpeed);
+
+        animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
+        rb.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
+    }
+
+    
+}
